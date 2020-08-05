830aec12e672ad692c0ef18ddec33a82c6edacea
Gabao-Farias <gabrielfariasbass@gmail.com>
2020-08-05 10:24:47 -0300

    Improved view of data

--- src/view/index.js
+++ src/view/index.js
@@ -1,24 +1,47 @@
 import React, {Component} from 'react';
 
-import { PermissionsAndroid } from 'react-native';
+import { PermissionsAndroid, StatusBar, Image } from 'react-native';
 
 import Geolocation from '@react-native-community/geolocation';
 
 import api from '../services/weatherApi';
 
 import Icon from 'react-native-vector-icons/EvilIcons';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import {
     Container,
     LocationContainer,
     Location,
-    WeatherContainer,
     City,
-    CityName} from './styles';
+    CityName,
+    Refresh,
+    WeatherContainer,
+    Title,
+    Ambient,
+    SmallCard,
+    SmallCardDescription,
+    FastTest,
+    FastTestText,
+} from './styles';
 
 export default class App extends Component{
     state = {
-        weather: {},
+        weather: {
+            ambient: {
+                feels_like: 0,
+                humidity: 0,
+                temp: 0,
+                temp_max: 0,
+                temp_min: 0,
+                wind_speed: 0,
+            },
+            dayWeather: {
+                description: '',
+                icon: '',
+                main: '',
+            },
+        },
         location: {
             cityName: 'London',
         },
@@ -27,6 +50,8 @@ export default class App extends Component{
     refreshLocation(){
         Geolocation.getCurrentPosition(location => this.refreshWeather(location));
     }
+
+    
 
     async requestLocationPermission(){
         try{
@@ -43,6 +68,40 @@ export default class App extends Component{
         }
     }
 
+    async refreshWeatherByCityName(cityName){
+        try{
+            const response = await api.get(`/weather?q=${cityName}&lang=pt_br&appid=dfa9dceaeb9c36a193b24efaa4e27a76`);
+
+            const parsedResponse = JSON.parse(response.request._response);
+
+            console.log(parsedResponse);
+
+            this.setState({
+                weather: {
+                    ambient: {
+                        feels_like: parsedResponse.main.feels_like,
+                        humidity: parsedResponse.main.humidity,
+                        temp: parsedResponse.main.temp,
+                        temp_max: parsedResponse.main.temp_max,
+                        temp_min: parsedResponse.main.temp_min,
+                        wind_speed: parsedResponse.wind.speed,
+                    },
+                    dayWeather: {
+                        description: parsedResponse.weather[0].description.charAt(0).toUpperCase() + parsedResponse.weather[0].description.slice(1),
+                        icon: parsedResponse.weather[0].icon,
+                        main: parsedResponse.weather[0].main,
+                    }
+                },
+                location: {
+                    cityName: parsedResponse.name,
+                },
+            });
+        }catch(err){
+            console.log(err);
+        }
+
+    }
+
     async refreshWeather(location){
         try{
             const response = await api.get(`/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&lang=pt_br&appid=dfa9dceaeb9c36a193b24efaa4e27a76`);
@@ -52,9 +111,24 @@ export default class App extends Component{
             console.log(parsedResponse);
 
             this.setState({
+                weather: {
+                    ambient: {
+                        feels_like: parsedResponse.main.feels_like,
+                        humidity: parsedResponse.main.humidity,
+                        temp: parsedResponse.main.temp,
+                        temp_max: parsedResponse.main.temp_max,
+                        temp_min: parsedResponse.main.temp_min,
+                        wind_speed: parsedResponse.wind.speed,
+                    },
+                    dayWeather: {
+                        description: parsedResponse.weather[0].description.charAt(0).toUpperCase() + parsedResponse.weather[0].description.slice(1),
+                        icon: parsedResponse.weather[0].icon,
+                        main: parsedResponse.weather[0].main,
+                    }
+                },
                 location: {
                     cityName: parsedResponse.name,
-                }
+                },
             });
         }catch(err){
             console.log(err);
@@ -68,6 +142,7 @@ export default class App extends Component{
     render(){
         return(
             <Container>
+                <StatusBar backgroundColor="transparent" translucent barStyle="light-content" />
                 <LocationContainer>
                     <Location>
                         <Icon name="location" size={40} color="#fff"/>
@@ -75,11 +150,63 @@ export default class App extends Component{
                             <CityName>{this.state.location.cityName}</CityName>
                         </City>
                     </Location>
-                    <Icon name="refresh" size={40} color="#fff"/>
+                    <Refresh onPress={() => {this.refreshLocation()}}>
+                        <Icon name="refresh" size={40} color="#fff"/>
+                    </Refresh>
                 </LocationContainer>
                 <WeatherContainer>
+                    <Title>{this.state.weather.dayWeather.description}</Title>
+                    <Ambient>
+                        <SmallCard>
+                            <MaterialIcon name="thermometer-chevron-down" size={30} color="#fff" />
+                            <SmallCardDescription>{Math.round(this.state.weather.ambient.temp_min - 273.15)}°C</SmallCardDescription>
+                        </SmallCard>
+                        <SmallCard>
+                            <MaterialIcon name="thermometer" size={30} color="#fff" />
+                            <SmallCardDescription>{Math.round(this.state.weather.ambient.temp - 273.15)} °C</SmallCardDescription>
+                        </SmallCard>
+                        <SmallCard>
+                            <MaterialIcon name="thermometer-chevron-up" size={30} color="#fff" />
+                            <SmallCardDescription>{Math.round(this.state.weather.ambient.temp_max - 273.15)} °C</SmallCardDescription>
+                        </SmallCard>
+                        <SmallCard>
+                            <MaterialIcon name="water-outline" size={30} color="#fff" />
+                            <SmallCardDescription>{this.state.weather.ambient.humidity}%</SmallCardDescription>
+                        </SmallCard>
+                        <SmallCard>
+                            <MaterialIcon name="human-handsdown" size={30} color="#fff" />
+                            <SmallCardDescription>{Math.round(this.state.weather.ambient.feels_like - 273.15)} °C</SmallCardDescription>
+                        </SmallCard>
+                        <SmallCard>
+                            <MaterialIcon name="weather-windy" size={30} color="#fff" />
+                            <SmallCardDescription>{Math.round(this.state.weather.ambient.wind_speed * 1.609)} Km/h</SmallCardDescription>
+                        </SmallCard>
+                    </Ambient>
+                </WeatherContainer>
+
+                <WeatherContainer>
+
+                    <Title>Testes rápidos</Title>
+
+                    <FastTest onPress={() => {this.refreshWeatherByCityName('Los Angeles')}}>
+                        <Icon name="location" size={40} color="#fff"/>
+                        <FastTestText>Los Angeles</FastTestText>
+                    </FastTest>
+                    <FastTest onPress={() => {this.refreshWeatherByCityName('Moscou')}}>
+                        <Icon name="location" size={40} color="#fff"/>
+                        <FastTestText>Moscou</FastTestText>
+                    </FastTest>
+                    <FastTest onPress={() => {this.refreshWeatherByCityName('Paris')}}>
+                        <Icon name="location" size={40} color="#fff"/>
+                        <FastTestText>Paris</FastTestText>
+                    </FastTest>
+                    <FastTest onPress={() => {this.refreshWeatherByCityName('Tokyo')}}>
+                        <Icon name="location" size={40} color="#fff"/>
+                        <FastTestText>Tokyo</FastTestText>
+                    </FastTest>
                     
                 </WeatherContainer>
+
             </Container>
         );
     }
