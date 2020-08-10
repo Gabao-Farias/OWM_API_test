c5d4ba341261a6b0568d734684d66d5bc03a0364
Gabao-Farias <gabrielfariasbass@gmail.com>
2020-08-10 01:34:26 -0300

    Added params in Api call to auto convert data

--- src/components/WeatherInfo/index.js
+++ src/components/WeatherInfo/index.js
@@ -18,7 +18,7 @@ import {
 export default function WeatherInfo({info}){
     return(
         <WeatherContainer>
-            <CityName>{info.location.cityName}</CityName>
+            <CityName>{info.weather.location.cityName}</CityName>
             <Title>{info.weather.dayWeather.description}</Title>
 
             <WeatherImage>
@@ -31,32 +31,32 @@ export default function WeatherInfo({info}){
             <Ambient>
                 <SmallCard>
                     <MaterialIcon name="thermometer-chevron-down" size={30} color="#fff" />
-                    <SmallCardDescription>{Math.round(info.weather.ambient.temp_min - 273.15)}°C</SmallCardDescription>
+                    <SmallCardDescription>{Math.round(info.weather.ambient.temp_min) + "°C"}</SmallCardDescription>
                     <SmallerDescription>Mínima</SmallerDescription>
                 </SmallCard>
                 <SmallCard>
                     <MaterialIcon name="thermometer" size={30} color="#fff" />
-                    <SmallCardDescription>{Math.round(info.weather.ambient.temp - 273.15)} °C</SmallCardDescription>
+                    <SmallCardDescription>{Math.round(info.weather.ambient.temp) + "°C"} </SmallCardDescription>
                     <SmallerDescription>Média</SmallerDescription>
                 </SmallCard>
                 <SmallCard>
                     <MaterialIcon name="thermometer-chevron-up" size={30} color="#fff" />
-                    <SmallCardDescription>{Math.round(info.weather.ambient.temp_max - 273.15)} °C</SmallCardDescription>
+                    <SmallCardDescription>{Math.round(info.weather.ambient.temp_max) + "°C"} </SmallCardDescription>
                     <SmallerDescription>Máxima</SmallerDescription>
                 </SmallCard>
                 <SmallCard>
                     <MaterialIcon name="water-outline" size={30} color="#fff" />
-                    <SmallCardDescription>{info.weather.ambient.humidity}%</SmallCardDescription>
+                    <SmallCardDescription>{info.weather.ambient.humidity + "%"}</SmallCardDescription>
                     <SmallerDescription>Humidade do ar</SmallerDescription>
                 </SmallCard>
                 <SmallCard>
                     <MaterialIcon name="human-handsdown" size={30} color="#fff" />
-                    <SmallCardDescription>{Math.round(info.weather.ambient.feels_like - 273.15)} °C</SmallCardDescription>
+                    <SmallCardDescription>{Math.round(info.weather.ambient.feels_like) + "°C"} </SmallCardDescription>
                     <SmallerDescription>Sensação térmica</SmallerDescription>
                 </SmallCard>
                 <SmallCard>
                     <MaterialIcon name="weather-windy" size={30} color="#fff" />
-                    <SmallCardDescription>{Math.round(info.weather.ambient.wind_speed * 1.609)} Km/h</SmallCardDescription>
+                    <SmallCardDescription>{Math.round(info.weather.ambient.wind_speed) + " Km/h"}</SmallCardDescription>
                     <SmallerDescription>Vento</SmallerDescription>
                 </SmallCard>
             </Ambient>
